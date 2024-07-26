70aa712b3cfde0b7d5e8d9ebafa5625005058c44
sajid5544 <ghaffarakah7853@gmail.com>
2024-07-26 22:23:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923165123719"
+global.ownernomer = "923168436392"
 global.ownername = "Umar Rehman"
 global.ytname = "DarkDevil404"
 global.socialm = "GitHub: Um4r719"
 global.location = "Chaklala Scheme 3 Rawalpindi,Pakistan"
 
-global.ownernumber = '923165123719'  //creator number
+global.ownernumber = '923168436392'  //creator number
 global.ownername = '𝙐 𝙈 𝘼 𝙍 💛' //owner name
 global.botname = '𝙐𝘿 𝘽𝙐𝙂' //name of the bot
 
@@ -23,7 +23,7 @@ global.themeemoji = '🪀'
 global.wm = "Dark Devil"
 
 //theme link
-global.link = 'https://wa.me/+923165123719'
+global.link = 'https://wa.me/+923168436393'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
